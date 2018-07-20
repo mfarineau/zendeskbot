c478da36c3b029fcaf3e85c9c643aa460f37bbae
Matt Farineau <mfarineau@gmail.com>
2018-07-20 15:43:25 -0400

    try updating second scrolling

--- zendeskbot.py
+++ zendeskbot.py
@@ -39,10 +39,6 @@ ticketids = []
 # set a variable to ensure the autoscroll function is only called once
 scroll_limit = 0
 
-float_sec = (time.time() % 60) / 59.0
-
-seconds_progress = float_sec * 15
-
 ##################
 # api connection #
 ##################
@@ -145,8 +141,38 @@ while True:
 
     if numberoftickets == 0:
 
-        scrollphathd.set_pixel(int(seconds_progress), 6, brightness=0.3)
+        # Grab the "seconds" component of the current time
+        # and convert it to a range from 0.0 to 1.0
+        float_sec = (time.time() % 60) / 59.0
 
+        # Multiply our range by 15 to spread the current
+        # number of seconds over 15 pixels.
+        #
+        # 60 is evenly divisible by 15, so that
+        # each fully lit pixel represents 4 seconds.
+        #
+        # For example this is 28 seconds:
+        # [x][x][x][x][x][x][x][ ][ ][ ][ ][ ][ ][ ][ ]
+        #  ^ - 0 seconds                59 seconds - ^
+        seconds_progress = float_sec * 15
+
+        if DISPLAY_BAR:
+            # Step through 15 pixels to draw the seconds bar
+            for y in range(15):
+                # For each pixel, we figure out its brightness by
+                # seeing how much of "seconds_progress" is left to draw
+                # If it's greater than 1 (full brightness) then we just display 1.
+                current_pixel = min(seconds_progress, 1)
+
+                # Multiply the pixel brightness (0.0 to 1.0) by our global brightness value
+                scrollphathd.set_pixel(y + 1, 6, current_pixel * BRIGHTNESS)
+
+                # Subtract 1 now we've drawn that pixel
+                seconds_progress -= 1
+
+                # If we reach or pass 0, there are no more pixels left to draw
+                if seconds_progress <= 0:
+                    break
 
     # Call the autoscroll function that will send the pi buffer to the led screen, but make sure it only runs once
     while scroll_limit < 1:
